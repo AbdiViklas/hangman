7d714a317f56332c02e7d233bd4145a3c58f1087
Andy Bonner <abonner@duke.edu>
2017-05-30 12:36:34 -0400

    add regex catch for non-alpha guesses

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let wordList = ["tight","flavor","pass","navy","peasant","ridge","slump","outloo
 let svgElements = ['<circle fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" cx="181.032199" cy="90.062339" r="52.325901" id="head"/>',
 	'<line fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x1="181.032199" y1="144.062339" x2="182.032199" y2="301.062339" id="torso"/>',
 	' <line fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x1="121.425061" y1="157.419516" x2="180.353635" y2="216.34809" id="left_arm"/>',
-	'<line fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x1="180.353632" y1="159.205231" x2="239.282206" y2="218.133805" id="right_arm" transform="rotate(90 209.8179168701172,188.66950988769534) "/>'
+	'<line fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x1="180.353632" y1="159.205231" x2="239.282206" y2="218.133805" id="right_arm" transform="rotate(90 209.8179168701172,188.66950988769534) "/>',
 	'<line fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x1="122.317914" y1="302.955229" x2="181.246488" y2="361.883803" id="left_leg" transform="rotate(90 151.78219604492185,332.41952514648443) "/>',
 	'<line fill="none" stroke="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" x1="182.121835" y1="302.955229" x2="241.050409" y2="361.883803" id="right_leg"/>'
 ];
@@ -24,7 +24,7 @@ let pickWord = () => {
 let wrongGuess = letter => {
 	document.getElementById("wrongGuesses").innerHTML += " " + letter;
 	// next: draw man
-	alreadyGuessed.push(letter);
+	alreadyGuessed.push(letter)
 }
 
 let rightGuess = letter => {
@@ -35,8 +35,10 @@ window.onload = pickWord();
 
 document.onkeyup = event => {
 	let userGuess = event.key.toLowerCase();
-	if (!/[a-z]/.test(userGuess)) {
+	console.log(userGuess);
+	if (!(/^[a-z]$/).test(userGuess)) {
 		alert("Letter keys only, please.");
+		return;
 	} else if (alreadyGuessed.includes(userGuess)) {
 		return;
 	} else if (!mysteryWord.includes(userGuess)) {
